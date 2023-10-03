39418b41e79ecc1dbeae6d1dcf3fe76e3c7decd3
saisankp <prathameshsaisankar@Gmail.com>
2023-10-03 03:54:43 +0100

    Initial UI for text input

--- TestPilot/README.md
+++ TestPilot/README.md
@@ -1,3 +1,7 @@
 # TestPilot README
 
 This directory contains code for the Visual Studio Code extension written in Typescript. This extension communicates with a backend through a RESTful API to generate tests.
+
+# How to run this extension locally
+1. Run `npm run watch` to monitor changes in files as you develop.
+2. Press `F5` while on extension.ts to run the extension in a new Visual Studio Code window through the "Extension Development Host".
--- TestPilot/src/extension.ts
+++ TestPilot/src/extension.ts
@@ -1,30 +1,12 @@
 import * as vscode from 'vscode';
+import { SidebarProvider } from './SidebarProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-    // Register command to show the side panel
-    let disposable = vscode.commands.registerCommand('extension.showHelloWorldPanel', () => {
-        // Create and show a WebviewPanel
-        const panel = vscode.window.createWebviewPanel(
-            'helloWorldPanel',
-            'Hello World',
-            vscode.ViewColumn.One,
-            {
-                enableScripts: true
-            }
-        );
-
-        // Set the HTML content for the WebviewPanel
-        panel.webview.html = getWebviewContent();
-    });
-
-    // Add the command to the context subscriptions
-    context.subscriptions.push(disposable);
+	const sidebar = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			"testpilot-sidebar",
+			sidebar
+		)
+	);
 }
-
-function getWebviewContent() {
-    return `
-        <h1>Hello World!</h1>
-    `;
-}
-
-export function deactivate() {}
